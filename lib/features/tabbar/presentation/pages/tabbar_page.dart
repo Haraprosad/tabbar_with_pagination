2a4bd7d465085f924ddb64ee301c3ed091e776a1
iamniloy <padma.haraprosad@gmail.com>
2022-04-05 15:43:43 +0600

    initial commit

---
+++ lib/features/tabbar/presentation/pages/tabbar_page.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+
+class TabbarPage extends StatefulWidget {
+  const TabbarPage({Key? key}) : super(key: key);
+
+  @override
+  _TabbarPageState createState() => _TabbarPageState();
+}
+
+class _TabbarPageState extends State<TabbarPage> with TickerProviderStateMixin{
+  @override
+  void dispose() {
+    super.dispose();
+  }
+  @override
+  Widget build(BuildContext context) {
+     final _tabController = TabController(length: 3, vsync: this);
+    return Scaffold(
+      appBar: AppBar(title: const Text("TabBar View"),),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Container(height: 50,
+            color: Colors.grey,),
+            Container(
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: TabBar(
+                  controller: _tabController,
+                  //isScrollable: true,
+                  labelColor: Colors.deepOrange,
+                  indicatorColor: Colors.blue,
+                  unselectedLabelColor: Colors.grey,
+                  labelPadding: const EdgeInsets.only(left: 20,right: 20),
+                  padding: const EdgeInsets.only(bottom: 10,),
+                  indicator: const CircleTabIndicator(color: Colors.deepOrange,radius: 4),
+                  tabs: const [
+                    Tab(text: "Tab 1",),
+                    Tab(text: "Tab 2",),
+                    Tab(text: "Tab 3",)
+                  ],
+                ),
+              ),
+            ),
+            Container(
+              height: 400,
+              width: double.maxFinite,
+              child:  TabBarView(
+                controller: _tabController,
+                children: const [
+                  Center(child: Text("Tab 1 Data")),
+                  Center(child: Text("Tab 2 Data")),
+                  Center(child: Text("Tab 3 Data")),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+///***********************Circle indicator -start**********
+class CircleTabIndicator extends Decoration {
+  final Color color;
+  final double radius;
+
+  const CircleTabIndicator({required this.color, required this.radius});
+  //override createBoxPainter
+  @override
+  BoxPainter createBoxPainter([VoidCallback? onChanged]) {
+    return CirclePainter(radius: radius, color: color);
+  }
+}
+
+class CirclePainter extends BoxPainter {
+  final double radius;
+  late Color color;
+  CirclePainter({required this.radius,required this.color});
+  @override
+  void paint(Canvas canvas, Offset offset, ImageConfiguration cfg) {
+    late Paint _paint;
+    _paint = Paint()..color = color;
+    _paint = _paint..isAntiAlias = true;
+    final Offset circleOffset = offset + Offset(cfg.size!.width/2,cfg.size!.height-radius);
+    canvas.drawCircle(circleOffset, radius, _paint);
+  }}
+///***********************Circle indicator -end************
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:tabbar_with_pagination/app.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
